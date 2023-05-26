2816b32ed351be8440a1847b0e0426a964059c87
Sokozmat <smerekabeats@gmail.com>
2023-05-26 12:59:26 +0200

    Initial Commit

---
+++ HelloWorldApp/HelloWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppApp.swift
+//  HelloWorldApp
+//
+//  Created by Smereka on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
